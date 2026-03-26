8aa79e06e5015392732acadbbca27e154f888663
OFCLeoS <leandroptfr123@outlook.com>
2026-03-26 15:26:31 +0100

    20:feat(wave): Created base classes for the Wave System

---
+++ Assets/Scripts/Wave System/Objectives/WaveObjective.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class WaveObjective
+{
+    [SerializeField] protected float waveTime;
+    
+}
---
+++ SPEC/SPEC_WAV-01-WaveSystem_v1.0.md
@@ -0,0 +1,4 @@
+# WAV-01 - Wave System
+## Difficulty Scaling
+
+## Wave Objectives
