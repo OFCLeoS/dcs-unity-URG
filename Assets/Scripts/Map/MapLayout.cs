d31a3fcd9c8b07ee95ff1a21855e21ad990a9fe0
OFCLeoS <leandroptfr123@outlook.com>
2026-05-12 19:00:43 +0200

    36:feat(map): Finished map translator

--- Assets/Scripts/Map/MapLayout.cs
+++ Assets/Scripts/Map/MapLayout.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MapLayout : MonoBehaviour
 {
-    MapRandomSector[] mapSections;
+    List<MapRandomSector> mapSections = new List<MapRandomSector>();
+
+    public void AddMapSection(MapRandomSector randomSector)
+    {
+        mapSections.Add(randomSector);
+    }
 }
